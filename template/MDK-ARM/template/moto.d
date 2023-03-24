296c2f0cf1fa78150ceb1d1ad449840ea739787c
JiaLong liu <108475864+JIAlonglong@users.noreply.github.com>
2023-03-24 11:35:04 +0800

    Revert "22 (#6)" (#7)
    This reverts commit bc28453473ca296236b96d2ffd6e85e630d06ab4.

--- template/MDK-ARM/template/moto.d
+++ template/MDK-ARM/template/moto.d
@@ -3,7 +3,7 @@ template\moto.o: ..\GDUT_User\moto.h
 template\moto.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
 template\moto.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
 template\moto.o: ../Drivers/CMSIS/Include/core_cm4.h
-template\moto.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+template\moto.o: D:\Software\keil5\ARM\ARMCC\Bin\..\include\stdint.h
 template\moto.o: ../Drivers/CMSIS/Include/cmsis_version.h
 template\moto.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 template\moto.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -15,7 +15,7 @@ template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
 template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
 template\moto.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
 template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-template\moto.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+template\moto.o: D:\Software\keil5\ARM\ARMCC\Bin\..\include\stddef.h
 template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
 template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
 template\moto.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
@@ -38,7 +38,7 @@ template\moto.o: ../Core/Inc/main.h
 template\moto.o: ../GDUT_User/MoveBase.h
 template\moto.o: ../Middlewares/ST/ARM/DSP/Inc/arm_math.h
 template\moto.o: ../Drivers/CMSIS/Include/core_cm4.h
-template\moto.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-template\moto.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+template\moto.o: D:\Software\keil5\ARM\ARMCC\Bin\..\include\string.h
+template\moto.o: D:\Software\keil5\ARM\ARMCC\Bin\..\include\math.h
 template\moto.o: ../Core/Inc/can.h
 template\moto.o: ../GDUT_User/elmo.h
